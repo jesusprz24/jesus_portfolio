36fcb465ab8dbb45cfd19a14410d243ce3902446
Jesus <perez.jesus1039@gmail.com>
2023-04-17 12:18:58 -0700

    created the data folder for the projects

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+      title: "WhataCrypto",
+      subtitle: "Crypto Game",
+      description:
+        "A crypto currency game designed to show beginners the best time to buy and sell crypto currency. Using real time crypto the users job is to see how much they make or lose",
+      image: "./whatacrypto.png",
+    //   link: " ",
+    },
+    {
+      title: "Baconator",
+      subtitle: "Kevin Baconator",
+      description:
+        "While the name is a bit humorous and does sound like the Wendy's burger it is not. Baconator is actually an app that allows users to share thoughts and comments about the movies of Kevin Bacon",
+      image: ".//baconator.png",
+    //   link: "",
+    },
+    {
+      title: "Dungeons and Dragons Generator",
+      subtitle: "Character Generator",
+      description:
+        "This character generator is meant for beginners of the DnD world. Often times it is difficult for new commers to come up with character stats. This application makes it easy with the click of a button",
+      image: "./character_gen.png",
+    //   link: "",
+    },
+  ];
