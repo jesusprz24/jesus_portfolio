c3624e607064e01e1ad7837bde98bc649e78a2ed
Jesus <perez.jesus1039@gmail.com>
2023-04-16 21:02:48 -0700

    finished the code for the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,59 @@ import React from "react";
 import Navbar from "./Navbar";
 
 function Contact() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <div>Contact</div>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold mb-4">Contact Me</h1>
+        <form className="w-1/2">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="name" className="text-lg mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="border border-gray-400 rounded-lg px-4 py-2"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email" className="text-lg mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="border border-gray-400 rounded-lg px-4 py-2"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="message" className="text-lg mb-2">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="border border-gray-400 rounded-lg px-4 py-2 h-32"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
-  
-  export default Contact;
+
+export default Contact;
+
   
