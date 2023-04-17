67f5c63f04f1579759aa2b003bd821acc984812b
Jesus <perez.jesus1039@gmail.com>
2023-04-16 20:40:27 -0700

    adding links to the navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import About from './components/About';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "about",
+    element: <About />
+  },
+  // Add additional route objects here for other URLs
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <Route path="/" element={<App />} />
+      {}
+    </RouterProvider>
   </React.StrictMode>
 );
