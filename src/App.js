c82ca45f4bbd5c7ca2d28ca0f0ba9bab56241bcd
Jesus <perez.jesus1039@gmail.com>
2023-04-16 20:49:19 -0700

    finally got the navbar to work without errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import AboutMe from './components/AboutMe';
+/*import About from './components/About';*/
 
 
 function App() {
   return (
     <div>
       <Navbar />
-      <AboutMe />
+      {/* <About /> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-[#2d3e50] text-white px-10 py-10 mb-4 flex flex-col md:flex-row justify-between items-center w-[100vw]">
-    <Link to="/">
-        <h1 className="text-2xl">Jesus Perez</h1>
-    </Link>
-    <div className='flex justify-between items-center mt-2 md:mt-0'>
-        <Link to="/about" className="underline text-white hover:text-blue-600 mr-4">About Me</Link>
-        <Link to="/spells" className="underline text-white hover:text-blue-600 mr-4">Projects</Link>
-        <Link to="/game" className="underline text-white hover:text-blue-600 mr-4">Social Media</Link>
-        <Link to="/contact" className="underline text-white hover:text-blue-600 mr-4">Contact</Link>
+    <div>
+      <div>
+        Jesus Perez Alvarado
+      </div>
+      <ul>
+      <li>Home</li>
+      <li><Link to="/about">About</Link></li>
+      <li>Projects</li>
+      <li>Social</li>
+      <li>Contact</li>
+      </ul>
     </div>
-</nav>
-  );
+  )
 }
 
 
