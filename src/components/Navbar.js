377560a2060f8ba5133347bfcb32fceb712f6262
Jesus <perez.jesus1039@gmail.com>
2023-04-17 13:03:46 -0700

    re-created the navbar to make it more functioning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,33 @@
+import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
-import { Link } from "react-router-dom";
 
-function Navbar() {
+export default function Navbar() {
   return (
-    <div className="bg-gray-900 text-white">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-xl font-bold">Jesus Perez Alvarado</div>
-        <ul className="flex space-x-4">
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500 transition-colors duration-300"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className="hover:text-blue-500 transition-colors duration-300"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/projects"
-              className="hover:text-blue-500 transition-colors duration-300"
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/social"
-              className="hover:text-blue-500 transition-colors duration-300"
-            >
-              Social Media
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="hover:text-blue-500 transition-colors duration-300"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Jesus Perez Alvarado
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Past Work
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+          <a href="#testimonials" className="mr-5 hover:text-white">
+            Testimonials
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          Hire Me
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a>
       </div>
-    </div>
+    </header>
   );
 }
-
-export default Navbar;
