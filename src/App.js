25fa16c3a34767f77c2bc62d12d865efcaaed09e
Jesus <perez.jesus1039@gmail.com>
2023-04-17 11:34:17 -0700

    need to change the structure to make it work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-/*import About from './components/About';*/
-
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Social from './components/Social';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      {/* <About /> */}
-    </div>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/social" component={Social} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
 }
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import About from './components/About';
-import Contact from './components/Contact';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "about",
-    element: <About />
-  },
-  {
-    path: "contact",
-    element: <Contact />
-  },
-  // Add additional route objects here for other URLs
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <Route path="/" element={<App />} />
-      {}
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
+
